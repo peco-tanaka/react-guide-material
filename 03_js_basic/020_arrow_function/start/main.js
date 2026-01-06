17140e83f42054c65863d06268960694d066cda8
peco-tanaka <ok.computeruk1929@gmail.com>
2026-01-07 02:05:00 +0900

    feat: アロー関数

--- 03_js_basic/020_arrow_function/start/main.js
+++ 03_js_basic/020_arrow_function/start/main.js
@@ -1,3 +1,27 @@
+// 関数式
 function fn(number) {
   return number * 2;
 }
+
+console.log(fn(2));
+
+// 無名関数
+const fn = function(number) {
+  return number * 2;
+}
+
+// アロー関数
+const fnArrow = (number) => {
+  return number * 2;
+}
+
+// 引数が一つの場合、()を省略できる。1行の場合、returnは省略できる
+const fnArrow2 = number => {
+  return number * 2;
+}
+
+// オブジェクトを返すときは()で囲う
+const fnArrowObj = number => ({ result: number * 2 });
+
+
+console.log(fnArrow(2));
