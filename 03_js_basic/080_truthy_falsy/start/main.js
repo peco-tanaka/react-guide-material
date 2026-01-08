a89e8b42dfc18718c52a28f5b7a07a7b14897ef3
peco-tanaka <ok.computeruk1929@gmail.com>
2026-01-08 20:56:53 +0900

    feat: truthyとfalsy. 論理積は最初にfalseを返し、論理和は最初にtrueを返す

--- 03_js_basic/080_truthy_falsy/start/main.js
+++ 03_js_basic/080_truthy_falsy/start/main.js
@@ -13,23 +13,26 @@ NaN (Not a Number)
 
 const a = 0;
 let result = a ? 10 : -10;
-console.log(result);
+// console.log(result);
 
 const falsy = 0;
 const truthy = 1;
-console.log(Boolean(truthy));
-console.log(Boolean(falsy));
+// console.log(Boolean(truthy));
+// console.log(Boolean(falsy));
 
 // 論理積 (&&) について
+// 左から順番にfalsyな値を見つけた時点で、その値を格納する
+// falthyな値が見つからなかったときは、一番右の値を格納する
 const resultA = "" && "foo";
 const resultB = 2 && 1 && 0 && 3;
 const resultC = "foo" && 4;
 
-// console.log(resultA);
-// console.log(resultB);
-// console.log(resultC);
+console.log(resultA);
+console.log(resultB);
+console.log(resultC);
 
 // 理論和 (||) について
+// 左から順番にtruthyな値を見つけた時点で、その値を格納する
 const resultD = "" || "foo";
 const resultE = 0 || 2 || 0;
 const resultF = "foo" || 4;
