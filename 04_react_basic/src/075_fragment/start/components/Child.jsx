4b6b4e106531d1e2d476dcb136e1898b6bd4ca03
peco-tanaka <ok.computeruk1929@gmail.com>
2026-01-12 04:21:34 +0900

    feat: 親要素を二つ並べる時にはFlagment<>を使用する

--- 04_react_basic/src/075_fragment/start/components/Child.jsx
+++ 04_react_basic/src/075_fragment/start/components/Child.jsx
@@ -2,9 +2,15 @@ import "./Child.css";
 
 const Child = () => {
   return (
-    <div className="component">
-      <h3>Hello Component</h3>
-    </div>
+    // 余計なタグを出したくない時にFragmentで囲う。html変換した時にはなかったものとして扱われる
+    // key属性のみつけられる。その時は <Fragment> と記述する必要がある
+    <>
+      <div className="component">
+        <h3>Hello Component</h3>
+      </div>
+      <h3>Hello Fragment</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor cupiditate ea optio officiis tempora, voluptatum sit eaque iure aliquid, hic, nulla vero veritatis sed autem temporibus consectetur necessitatibus culpa praesentium.</p>
+    </>
   );
 };
 
