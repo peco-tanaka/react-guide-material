72a7216d09e9ce915d6910550c638f527096b06e
peco-tanaka <ok.computeruk1929@gmail.com>
2026-01-14 03:13:19 +0900

    feat: props. propsのプロパティ、関数、真偽値、オブジェクトをデフォルト値ありで渡す

--- 04_react_basic/src/090_props/start/Example.jsx
+++ 04_react_basic/src/090_props/start/Example.jsx
@@ -1,9 +1,21 @@
 import Child from "./components/Child";
 
 const Example = () => {
+  const hello = (arg) => `Hello ${arg}`;
+  const o = {
+    color: "red",
+    num: 123
+  }
+
   return (
     <>
       <Child />
+      <Child
+        {...o}
+        fn={hello}
+        bool
+        obj={{ name: 'Tom', age: 18 }}
+      />
       <Child color=""/>
       <Child color="red"/>
     </>
--- 04_react_basic/src/090_props/start/components/Child.jsx
+++ 04_react_basic/src/090_props/start/components/Child.jsx
@@ -6,14 +6,19 @@
 import "./Child.css";
 
 // オブジェクトなので分割代入できる
-const Child = ({ color: c = 'green' }) => {
+const Child = ({ color: c = 'green', num = 123, fn = (arg) => arg, bool = false, obj = { name: 'Momo', age: 10 }}) => {
 
   // 呼び出し元のpropsで渡された値の中身を確認
   // console.log(props)
+  console.log(num)
 
   return (
     <div className={`component ${c}`}>
       <h3>Hello Component</h3>
+      <h3>{num}</h3>
+      <h3>{fn('Props')}</h3>
+      <h3>{ bool ? 'true' : 'false' }</h3>
+      <h3>{ obj.name + obj.age }</h3>
     </div>
   );
 };
