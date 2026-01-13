a2f3ab4382d5c7f8f71bdb75eb6b4bc09f6e5013
peco-tanaka <ok.computeruk1929@gmail.com>
2026-01-14 02:48:12 +0900

    feat: propsで値を渡す

--- 04_react_basic/src/090_props/start/components/Child.jsx
+++ 04_react_basic/src/090_props/start/components/Child.jsx
@@ -5,9 +5,14 @@
 
 import "./Child.css";
 
-const Child = () => {
+// オブジェクトなので分割代入できる
+const Child = ({ color: c = 'green' }) => {
+
+  // 呼び出し元のpropsで渡された値の中身を確認
+  // console.log(props)
+
   return (
-    <div className="component">
+    <div className={`component ${c}`}>
       <h3>Hello Component</h3>
     </div>
   );
